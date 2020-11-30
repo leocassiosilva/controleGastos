aa1fd1eb5055bcf59d99f0b84439058d4734bc20
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-29 21:27:00 -0300

    model categoria

--- contas/views.py
+++ contas/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 import datetime
 
 def home(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    #html = "<html><body>It is now %s.</body></html>" % now
+    return render(request, 'contas/home.html')

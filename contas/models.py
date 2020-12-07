2184f4b80495997357eb61049f929620078cc308
leocassiosilva <leocassiosilva1234@gmail.com>
2020-12-07 11:10:21 -0300

    modificando o tipo da data

--- contas/models.py
+++ contas/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.nome
 
 class Transacao(models.Model):
     data = models.DateTimeField()
@@ -16,3 +18,7 @@ class Transacao(models.Model):
 
     class Meta:
         verbose_name_plural = 'Transacoes'
+
+#serve para apresentar a descrição ao inves de todo o objeto
+    def __str__(self):
+        return self.descricao

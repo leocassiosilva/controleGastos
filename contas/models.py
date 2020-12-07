ed80bbc59c3e065d538762d8fa3238b227c1a541
leocassiosilva <leocassiosilva1234@gmail.com>
2020-12-07 10:48:38 -0300

    modificando o tipo da data

--- contas/models.py
+++ contas/models.py
@@ -8,11 +8,11 @@ class Categoria(models.Model):
     dt_criacao = models.DateTimeField(auto_now_add=True)
 
 class Transacao(models.Model):
-    data = models.DateTimeField(auto_now_add=True)
+    data = models.DateTimeField()
     descricao = models.CharField(max_length=200)
     valor = models.DecimalField(max_digits=7, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    observacoes = models.TextField()
+    observacoes = models.TextField(null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'Transacoes'
